5385d992d4af18ca9abefa9df8359301fb5d702f
andykumar412 <andykumar412@gmail.com>
2020-05-12 11:24:20 +0530

    reverse the number

---
+++ reverse.java
@@ -0,0 +1,33 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while(t-->0)
+		{
+		    int n = s.nextInt();
+		    System.out.println(reverse(n));
+		}
+	}
+	
+	public static int reverse(int n)
+	{
+	    String s ="";
+	    String s1 ="";
+	    s+=n;
+	    StringBuffer br = new StringBuffer();
+	    br.append(s);
+	    br.reverse();
+	    s1+=br;
+	    return Integer.parseInt(s1);
+	    
+	}
+}
